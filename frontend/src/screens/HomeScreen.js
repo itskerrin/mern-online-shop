fe1c71e8a84d8db1f39189896957952ee1d5a908
kerrin <itskerrin@gmail.com>
2021-08-22 13:53:26 +0100

    used axios to get data from the backend

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,26 @@
-import products from '../products';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+  /*  useEffect will load on the the page load. We cant make the useEffect an async await
+function so we can make inside a nested function that will be a/a. We use array destructuring to
+set data = to the products json and change the setProducts state to equal data.
+*/
+
   return (
     <>
       <h1>Latest Products</h1>
