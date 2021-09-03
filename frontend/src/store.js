e99296d8a681b50c2d4554db573a98db9d9fa4cf
kerrin <itskerrin@gmail.com>
2021-09-03 17:31:44 +0100

    added cart functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,12 +5,12 @@ import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducers';
-//import { cartReducer } from './reducers/cartReducers';
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  //cart: cartReducer,
+  cart: cartReducer,
 });
 
 // const cartItemsFromStorage = localStorage.getItem('cartItems')
@@ -20,11 +20,20 @@ const reducer = combineReducers({
 // // const initialState = {
 // //   cart: { cartItems: cartItemsFromStorage },
 // // };
+
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
+
 const middleware = [thunk];
 
 const store = createStore(
   reducer,
-  //initialState,
+  initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
 
